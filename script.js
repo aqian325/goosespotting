a3ce3953d835010fad33b44e832b2c44c5fe067c
aqian325 <aprilqian98@gmail.com>
2025-05-01 10:26:10 -0400

    interactions

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
-
-const cardImages = [
-  "card1.jpg", "card2.jpg","card5.jpg", "card6.jpg",
-  "card7.jpg", "card8.jpg", "card9.jpg", "card10.jpg", "card11.jpg", "card12.jpg",
-  "card13.jpg", "card14.jpg", "card15.jpg", "card16.jpg", 
-  "card17.jpg", "card18.jpg", "card19.jpg", "card20.jpg", "card21.jpg", "card22.jpg",
-  "card23.jpg", "card24.jpg", "card25.jpg", "card26.jpg", "card27.jpg", "card28.jpg",
-  "card29.jpg", "card30.jpg", "card31.jpg", "card32.jpg", "card33.jpg", "card34.jpg",
-  "card35.jpg", "card36.jpg", "card37.jpg", "card38.jpg", "card39.jpg", "card40.jpg",
-  "card41.jpg", "card42.jpg", "card43.jpg", "card44.jpg", "card45.jpg", "card46.jpg",
-  "card47.jpg", "card48.jpg", "card49.jpg", "card50.jpg", "card51.jpg", "card52.jpg",
-  "card53.jpg", "card54.jpg"
-];
-
-let currentIndex = 0;
+let currentIndex = 1; // card1 and card2 is the first front/back pair
+let flipState = false;
 
 function showCard(index) {
-  const img = document.getElementById("cardImage");
-  img.src = "cards/" + cardImages[index];
+  const front = document.getElementById("frontImage");
+  const back = document.getElementById("backImage");
+
+  front.src = `cards/card${index}.jpg`;
+  back.src = `cards/card${index + 1}.jpg`;
+
+  const card = document.getElementById("flipCardInner");
+  card.classList.remove("flipped");
+  flipState = false;
+}
+
+function flipCard() {
+  const card = document.getElementById("flipCardInner");
+  flipState = !flipState;
+  card.classList.toggle("flipped", flipState);
 }
 
 function nextCard() {
-  currentIndex = (currentIndex + 1) % cardImages.length;
+  currentIndex += 2;
+  if (currentIndex === 3) currentIndex = 5; // skip card15/card16
+  if (currentIndex > 53) currentIndex = 1;
   showCard(currentIndex);
 }
 
 function prevCard() {
-  currentIndex = (currentIndex - 1 + cardImages.length) % cardImages.length;
+  currentIndex -= 2;
+  if (currentIndex === 3) currentIndex = 1; // skip back over card15/card16
+  if (currentIndex < 1) currentIndex = 53;
   showCard(currentIndex);
 }
+
 
 window.onload = () => showCard(currentIndex);
