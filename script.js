0bf33ef9713781f063332060e0e10180714609e4
aqian325 <aprilqian98@gmail.com>
2025-05-01 10:09:22 -0400

    cards

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 const cardImages = [
-  "card1.jpg", "card2.jpg", "card3.jpg", "card4.jpg", "card5.jpg", "card6.jpg",
+  "card1.jpg", "card2.jpg","card5.jpg", "card6.jpg",
   "card7.jpg", "card8.jpg", "card9.jpg", "card10.jpg", "card11.jpg", "card12.jpg",
-  "card13.jpg", "card14.jpg",
+  "card13.jpg", "card14.jpg", "card15.jpg", "card16.jpg", 
   "card17.jpg", "card18.jpg", "card19.jpg", "card20.jpg", "card21.jpg", "card22.jpg",
   "card23.jpg", "card24.jpg", "card25.jpg", "card26.jpg", "card27.jpg", "card28.jpg",
   "card29.jpg", "card30.jpg", "card31.jpg", "card32.jpg", "card33.jpg", "card34.jpg",
